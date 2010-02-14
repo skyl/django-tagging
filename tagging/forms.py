6cd63d69335ce485a3cd98b96b5380009c65e975
Skylar Saveland <skylar.saveland@gmail.com>
2010-02-13 21:27:48 -0500

    validation on the admin form was hogwash

--- tagging/forms.py
+++ tagging/forms.py
@@ -13,15 +13,17 @@ class TagAdminForm(forms.ModelForm):
         model = Tag
 
     def clean_name(self):
+        ''' Hogwash
         value = self.cleaned_data['name']
         tag_names = parse_tag_input(value)
         if len(tag_names) > 1:
             raise forms.ValidationError(_('Multiple tags were given.'))
-        elif len(tag_names[0]) > settings.MAX_TAG_LENGTH:
+        if len(tag_names[0]) > settings.MAX_TAG_LENGTH:
             raise forms.ValidationError(
                 _('A tag may be no more than %s characters long.') %
                     settings.MAX_TAG_LENGTH)
-        return value
+        '''
+        return self.cleaned_data['name']
 
 class TagField(forms.CharField):
     """
